8c37596a5984c540ef34bd145fbdb59cdc93ec31
primChareka <primrose_chareka@hotmail.com>
2018-10-12 18:19:29 -0400

    added getService Methods

--- frontEnd/src/frontEnd/frontEndSystem.java
+++ frontEnd/src/frontEnd/frontEndSystem.java
@@ -1,14 +1,11 @@
 package frontEnd;
 
+import java.util.HashSet;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 /**
  * 
  * @author 
@@ -16,21 +13,17 @@ import java.util.StringTokenizer;
  */
 public class frontEndSystem {
 	// ----- Class Variables ----- //
-	private static ArrayList<Service> services = new ArrayList<>();
+	private static HashSet<String> validServices = new HashSet<String>();
+	private static boolean inPlannerMode = false;
 	private static Scanner scan = new Scanner(System.in);
-	
-	private static HashMap<String,Service> servicesMap = new HashMap<String,Service>();
-	// private boolean planner_mode = false;
 
 	// ----- Main Execution of Front End System ----- //
 	public static void main(String[] args) {
 		// When system starts, automatically ask to login
 		login();
 		
+		//After a successful login, load the valid services
 		frontEndSystem.loadValidServiceFile("./bin/frontEnd/ValidServices.txt");
-		for(String i : servicesMap.keySet()) {
-			System.out.println(i);
-		}
 		
 		String service_selection = "";
 		while (true) {
@@ -57,135 +50,118 @@ public class frontEndSystem {
 				changeTicket();
 				break;
 			default:
-				System.out.println("Error! Incorrect input please try again...");
+				System.out.println("Error! Invalid transaction type please try again...");
 			}
 		}
 	}
 
 	// ----- Methods ----- //
-	// Login to the system
+	/**
+	 * Waits for the user to enter login, and then determines the mode
+	 * of operation for the session (planner or agent mode). Sets the inPlannerMode
+	 * class boolean variable accordingly
+	 */
 	private static void login() {
-		String input = "";
-		System.out.println("Type 'login' to access the system...");
-
 		// Get input and check if correct for login
-		input = scan.nextLine();
+		System.out.println("Type 'login' to access the system...");
+		String input = scan.nextLine();
 		while (!input.equals("login")) {
-			System.out.println("Incorrect input! Please try again...");
+			System.out.println("Must login to use system! Please try again...");
 			input = scan.nextLine();
 		}
-
-		input = ""; // Reset input
 
 		// Get input to check for agent / planner mode
 		System.out.println("What mode would you like to use?");
 		input = scan.nextLine();
 		while (!input.equals("agent") && !input.equals("planner")) {
-			System.out.println("Incorrect input! Please try again...");
+			System.out.println("Invalid mode! Please try again...");
 			input = scan.nextLine();
 		}
 
-		// later, add a check to set planner mode if selected
-		// i.e if(input == "planner") { planner_mode = true; }
+		//Set flag for the mode which was selected
+		if(input.equals("planner")){ 
+			inPlannerMode = true; 
+		}
 
 		System.out.println("Logging in... Success!");
 		printMenu();
 	}
 
-	// Logout and end system
+	/**
+	 * Terminates the front end program with an exit status of 0
+	 */
 	private static void logout() {
 		System.out.println("Logging out... system will shut down...");
 		System.exit(0);
 	}
 
-	// Create a service
+	/**
+	 * Gathers the service number, capacity, name and date and logs transaction 
+	 * in the transaction summary file.
+	 * 
+	 * If the service number already exists returns to main without logging 
+	 * a transaction
+	 */
 	private static void createService() {
-		String input1 = "";
-		String input2 = "";
-		String input3 = "";
-
-		System.out.println("Please enter the service number");
-		input1 = scan.nextLine();
-
-		System.out.println("Please enter the service date");
-		input2 = scan.nextLine();
-
-		System.out.println("Please enter the service name");
-		input3 = scan.nextLine();
-
-		// Create service from inputs and add it to the services list
-		Service new_service = new Service(input1, input2, input3);
-		services.add(new_service);
-
-		System.out.println("Service successfully created!");
+		String serviceCode = "CRE";
+		String serviceNumber = getServiceNumber();
+		
+		if (serviceNumber!=null&&!validServices.contains(serviceNumber)) {
+			String serviceCapacity = getServiceCapacity();
+			String serviceName = getServiceName();
+			String serviceDate = getServiceDate();
+			
+			logTransaction(serviceCode, serviceNumber, serviceCapacity,null,serviceName, serviceDate);
+			System.out.println("Service successfully created!");
+		} else {		
+			System.out.println("That service number already exists!");
+		}
 		System.out.println("Returning to main menu...");
 	}
 
-	// Delete a service
+	/**
+	 * 
+	 */
 	private static void deleteService() {
-		String input1 = "";
-		String input2 = "";
-
-		System.out.println("Please enter the service number");
-		input1 = scan.nextLine();
-
-		System.out.println("Please enter the service name");
-		input2 = scan.nextLine();
-
-		// Find service to delete
-		int index = -1;
+		String serviceCode = "DEL";
+		String serviceNumber = getServiceNumber();
 		
-		/*if(servicesMap.containsKey(input1)&&servicesMap.get(input1).getServiceName().equals(input2)) {
-			servicesMap.remove(input1);
-		}*/
-		
-		for (int i = 0; i < services.size(); i++) {
-			if (services.get(i).getServiceNumber().equals(input1) && services.get(i).getServiceName().equals(input2)) {
-				index = i;
-				break;
-			}
-		}
+		if (validServices.contains(serviceNumber)) {
+			// Remove service from service list
+			validServices.remove(serviceNumber);
+			
+			String serviceName = getServiceName();
+			System.out.println(serviceName);
+			logTransaction(serviceCode, serviceNumber, null, null, serviceName, null);
 
-		// Ensure service was found and delete it if it was found
-		if (index != -1) {
-			// Figure out how we tell the backend to remove the service here
-			// STUFF
-
-			System.out.println("Deleted service number " + input1 + ": " + input2);
+			System.out.println("Deleted service number " + serviceNumber + ": " + serviceName);
 		} else {
-			System.out.println("Error! Did not find a matching service!");
-		}
-
+			System.out.println("Error! Did not find a matching service!");	
+		}	
+		
 		System.out.println("Returning to main menu...");
 	}
 
-	// Sell a ticket
+	/**
+	 * 
+	 */
 	private static void sellTicket() {
-		String input1 = "";
-		String input2 = "";
+		String serviceNumber = "";
+		String numTickets = "";
 
 		System.out.println("Please enter the service number");
-		input1 = scan.nextLine();
-
-		// Find service to sell tickets for
-		int index = -1;
-		for (int i = 0; i < services.size(); i++) {
-			if (services.get(i).getServiceNumber().equals(input1)) {
-				index = i;
-				break;
-			}
-		}
+		serviceNumber = scan.nextLine();
 
 		// Ensure service was found and sell tickets it if it was found
-		if (index != -1) {
+		if (validServices.contains(serviceNumber)) {
 			System.out.println("Please enter the number of tickets to sell");
-			input2 = scan.nextLine();
-
-			int number = Integer.parseInt(services.get(index).getNumberOfTickets());
-			number += Integer.parseInt(input2);
-			services.get(index).setNumberOfTickets(Integer.toString(number));
-
-			System.out.println("Sold " + input2 + "tickets for service " + input1);
+			numTickets = scan.nextLine();
+			
+			String transaction = "SEL" + " " + serviceNumber + " " + numTickets + " " + "00000" + " " + "*****" + " "
+					+ "*****";
+			logInTransactionSummaryFile(transaction);
+		
+			System.out.println("Sold " + numTickets + "tickets for service " + serviceNumber);
 		} else {
 			System.out.println("Error! Did not find a matching service!");
 		}
@@ -193,96 +169,67 @@ public class frontEndSystem {
 		System.out.println("Returning to main menu...");
 	}
 
-	// Cancel a ticket
+	/**
+	 * 
+	 */
 	private static void cancelTicket() {
-		String input1 = "";
-		String input2 = "";
-
 		System.out.println("Please enter the service number");
-		input1 = scan.nextLine();
-
-		// Find service to cancel tickets for
-		int index = -1;
-		for (int i = 0; i < services.size(); i++) {
-			if (services.get(i).getServiceNumber().equals(input1)) {
-				index = i;
-				break;
-			}
-		}
+		String serviceNumber = scan.nextLine();
 
 		// Ensure service was found and cancel tickets it if it was found
-		if (index != -1) {
+		if (validServices.contains(serviceNumber)) {
 			System.out.println("Please enter the number of tickets to cancel");
-			input2 = scan.nextLine();
+			String numTickets = scan.nextLine();
 
-			int number = Integer.parseInt(services.get(index).getNumberOfTickets());
-			number -= Integer.parseInt(input2);
-			services.get(index).setNumberOfTickets(Integer.toString(number));
+			//Log transaction in transaction summary file
+			String transaction = "CAN" + " " + serviceNumber + " " + numTickets + " " + "00000" + " " + "******" + " "
+					+ "*****";
+			logInTransactionSummaryFile(transaction);
 
-			System.out.println("Cancelled " + input2 + "tickets for service " + input1);
+			System.out.println("Cancelled " + numTickets + "tickets for service " + serviceNumber);
 		} else {
 			System.out.println("Error! Did not find a matching service!");
 		}
 
 		System.out.println("Returning to main menu...");
 	}
+	
 
 	// Change a ticket
+	/**
+	 * 
+	 */
 	private static void changeTicket() {
-		String input1 = "";
-		String input2 = "";
-		String input3 = "";
-
 		System.out.println("Please enter the current service number");
-		input1 = scan.nextLine();
+		String serviceNumber = scan.nextLine();
 
 		// Find service to change tickets for
-		int index1 = -1;
-		for (int i = 0; i < services.size(); i++) {
-			if (services.get(i).getServiceNumber().equals(input1)) {
-				index1 = i;
-				break;
-			}
-		}
-
-		// Ensure service was found and check for new service existance
-		if (index1 != -1) {
+		if(validServices.contains(serviceNumber)) {
 			System.out.println("Please enter the new service number");
-			input2 = scan.nextLine();
-
-			// Find service to change tickets to
-			int index2 = -1;
-			for (int i = 0; i < services.size(); i++) {
-				if (services.get(i).getServiceNumber().equals(input2)) {
-					index2 = i;
-					break;
-				}
-			}
-
-			// Ensure new service was found and then change tickets
-			if (index2 != -1) {
+			String serviceNumber2 = scan.nextLine();
+			if(validServices.contains(serviceNumber2)) {
 				System.out.println("Please enter the number of tickets to change");
-				input3 = scan.nextLine();
+				String numTickets = scan.nextLine();
 
-				int number = 0;
-				// Reduce tickets on existing service
-				number = Integer.parseInt(services.get(index1).getNumberOfTickets());
-				number -= Integer.parseInt(input3);
-				services.get(index1).setNumberOfTickets(Integer.toString(number));
-
-				// Increase tickets on new service
-				number = Integer.parseInt(services.get(index2).getNumberOfTickets());
-				number += Integer.parseInt(input3);
-				services.get(index2).setNumberOfTickets(Integer.toString(number));
-
-				System.out.println("Changed " + input3 + "tickets for service " + input1 + "to service" + input2);
+				//Log transaction in transaction summary file
+				String transaction = "CHG" + " " + serviceNumber + " " + numTickets + " " + serviceNumber2 + " " + "****" + " "
+						+ "*****";
+				logInTransactionSummaryFile(transaction);
+				System.out.println("Changed " + numTickets + "tickets for service " + serviceNumber + "to service" + serviceNumber2);
+			}else {
+				System.out.println("Error! Did not find a matching service!");
 			}
-		} else {
+		}else {
 			System.out.println("Error! Did not find a matching service!");
 		}
 
 		System.out.println("Returning to main menu...");
 	}
+	
+	/**
+	 * 
+	 * @param filePath
+	 */
 
 	// Load valid services list
 	private static void loadValidServiceFile(String filePath) {
@@ -294,11 +241,10 @@ public class frontEndSystem {
 			serviceNumber = fileReader.readLine();
 			
 			while(!serviceNumber.equals("00000")) {
-				//For Each entry add new service
+				//For each entry add new service
 				Service service = new Service();
 				service.setServiceNumber(serviceNumber);
-				services.add(service);
-				servicesMap.put(serviceNumber, service);
+				validServices.add(serviceNumber);
 				serviceNumber = fileReader.readLine();
 			}
 			fileReader.close();
@@ -309,6 +255,9 @@ public class frontEndSystem {
 		}
 	}
 
+	private static void logInTransactionSummaryFile(String transaction) {
+		System.out.println(transaction);	
+	}
 	// Print the menu for the user
 	private static void printMenu() {
 		System.out.println("----- MAIN MENU -----");
@@ -321,4 +270,53 @@ public class frontEndSystem {
 		System.out.println("changeTicket");
 	}
 
+	private static String getServiceNumber() {
+		System.out.println("Please enter the service number");
+		return scan.nextLine();
+	}
+	
+	private static String getServiceName() {
+		System.out.println("Please enter the service name");
+		return scan.nextLine().toLowerCase();
+	}
+	
+	private static String getServiceDate() {
+		System.out.println("Please enter the service date");
+		return scan.nextLine();
+	}
+	
+	private static String getServiceCapacity() {
+		System.out.println("Please enter the service capacity");
+		return scan.nextLine();
+	}
+	
+	private static String getNumberOfTickets() {
+		System.out.println("Please enter the number number");
+		return scan.nextLine();
+	}
+
+	private static void logTransaction(String code, String serviceNumber, String ticketsOrCapacity, String serviceNumber2, String serviceName, String serviceDate) {
+		
+		if(ticketsOrCapacity == null) {
+			ticketsOrCapacity = "0";
+		}
+		
+		if(serviceNumber2 == null) {
+			serviceNumber2 = "00000";
+		}
+		
+		if(serviceName == null) {
+			System.out.println(serviceName);
+			serviceName = "****";
+		}
+		
+		if(serviceDate == null) {
+			serviceDate = "****";
+		}
+		
+		String transaction = code + " " + serviceNumber + " " + ticketsOrCapacity + " " + serviceNumber2 + " " + serviceName
+				+ " " + serviceDate;
+		
+		logInTransactionSummaryFile(transaction);
+	}
 }
